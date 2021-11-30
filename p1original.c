0e6b1a856224c426951fed1e228a1cb7c00cf37e
Kavanahn <95006595+Kavanahn@users.noreply.github.com>
2021-11-30 03:15:52 +0000

    code

--- p1original.c
+++ p1original.c
@@ -1,2 +1,9 @@
-
+#include<stdio.h>
+int main()
+{
+int a, b, sum;
+scanf("%d %d", &a, &b);
+sum = a + b;
+printf("Sum of %d and %d is %d",a,b,sum);
+return 0; 
 }
---
+++ p2original.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int input_a()
+{
+  int a;
+  printf("enter the value of a");
+  scanf("%d %d")
+}
